5d2e9e7f323b3c0eb0ac057fdc86607b57088d56
Krishnakantha Upadhyaya <krishnakanth.kk96@gmail.com>
2023-01-16 12:46:02 +0530

    SVG part complete

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
 import styles from './CSS/Nav.module.css'
